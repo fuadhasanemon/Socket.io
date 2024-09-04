9643a3f9c264ddc23b3a2280ee47c394e164eb78
Fuad Hasan Emon <fuadhasanemon8@gmail.com>
2024-09-05 03:30:06 +0600

    Socket io chat app

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import colores from "colors";
 import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { readFileSync, writeFileSync } from "fs";
 
 // express init
 const app = express();
@@ -16,15 +17,24 @@ const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
   console.log(`Client connected succesfully`.bgGreen.black);
+  const recentData = JSON.parse(
+    readFileSync(path.join(__dirname, "db/data.json")).toString()
+  );
 
-  socket.send("I love JS");
+  io.sockets.emit("latest-msg", recentData);
 
-  socket.on("message", (data) => {
-    console.log(data);
-  });
+  socket.on("chat", ({ name, photo, message }) => {
+    const recentData = JSON.parse(
+      readFileSync(path.join(__dirname, "db/data.json")).toString()
+    );
+    recentData.push({ name, photo, message });
 
-  socket.on("disconnect", () => {
-    console.log(`Client disconnected!`.bgRed.white);
+    writeFileSync(
+      path.join(__dirname, "db/data.json"),
+      JSON.stringify(recentData)
+    );
+
+    io.sockets.emit("latest-msg", recentData);
   });
 });
 
